8ab24f78c38791e4368f9a7507f5d8ea0f7519b1
isaac <email@isaacpz.me>
2017-07-23 02:25:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# twilio-sounds
+
+twilio-sounds allows browser projects using the [twilio.js client](https://www.twilio.com/client) to package all necessary sound files instead of pulling them from the internet. This is especially helpful for chrome extensions which have strict content security policies.
+
+[![NPM Version][npm-image]][npm-url]
+
+## Install
+
+```bash
+npm i -S twilio-sounds
+```
+or
+```bash
+yarn add twilio-sounds
+```
+
+## Usage
+
+TODO
+
+## License
+
+[MIT](http://vjpr.mit-license.org)
+
+[npm-image]: https://img.shields.io/npm/v/twilio-sounds.svg
+[npm-url]: https://npmjs.org/package/twilio-sounds
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  outgoing: getSoundLocation("outgoing.mp3"),
+  disconnect: getSoundLocation("disconnect.mp3"),
+  dtmf1: getSoundLocation("dtmf-1.mp3"),
+  dtmf2: getSoundLocation("dtmf-2.mp3"),
+  dtmf3: getSoundLocation("dtmf-3.mp3"),
+  dtmf4: getSoundLocation("dtmf-4.mp3"),
+  dtmf5: getSoundLocation("dtmf-5.mp3"),
+  dtmf6: getSoundLocation("dtmf-6.mp3"),
+  dtmf7: getSoundLocation("dtmf-7.mp3"),
+  dtmf8: getSoundLocation("dtmf-8.mp3"),
+  dtmf9: getSoundLocation("dtmf-9.mp3"),
+  dtmf0: getSoundLocation("dtmf-0.mp3"),
+  dtmfh: getSoundLocation("dtmf-hash.mp3"),
+  dtmfs: getSoundLocation("dtmf-star.mp3")
+}
+
+getSoundLocation(fileName) {
+  return __dirname + "/assets/" + fileName;
+}
